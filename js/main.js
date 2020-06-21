4db7c4a78a39dd021237a7f97aa4a74568b8c673
Stuart Alexander <stuartalexander2@gmail.com>
2020-06-21 16:52:24 +0100

    This commit updates: mquery mixin and menu.

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ let filterInput = document.getElementById("search");
         
         for (let i = 0; i < li.length; i++){
           let a = li[i].getElementsByTagName("a")[0];
-          if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+          if(a.getAttribute('data-title').toUpperCase().indexOf(filterValue) > -1){
             li[i].style.display = '';
           } else {
             li[i].style.display = 'none';
