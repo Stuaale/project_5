1952f6f0278fb5c7f6c93380e4f1d1bb1f043d3e
Stuart Alexander <stuartalexander2@gmail.com>
2020-06-21 15:16:25 +0100

    Final Submission

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+  //Settings in place for lightbox options
+  
+  lightbox.option({
+      'resizeDuration' : 500,
+      'fadeDuration' : 1000,
+      'wrapAround' : true,
+      'alwaysShowNavOnTouchDevices': true
+  });
+
+  //This is the JS to support filter on search box
+
+let filterInput = document.getElementById("search");  
+    filterInput.addEventListener("keyup", imageNames);
+
+    function imageNames(){
+        let filterValue = document.getElementById("search").value.toUpperCase();
+        let ul = document.getElementById("image-list");
+        let li = ul.querySelectorAll("li.image-items");
+        
+        for (let i = 0; i < li.length; i++){
+          let a = li[i].getElementsByTagName("a")[0];
+          if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+            li[i].style.display = '';
+          } else {
+            li[i].style.display = 'none';
+          }
+        }
+    }   
+
+    
+
+
+  
+    
